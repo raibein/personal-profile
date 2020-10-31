0d7eb884fd01c7b0f02ccebc7005c5aa7d1999b2
raben <raben_x5@hotmail.com>
2020-10-31 22:28:00 +0100

    Components are added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	// Redirect,
+	Route,
+	Switch,
+} from 'react-router-dom';
+
 import './App.css';
+
+import DefaultContainer from './Containers/DefaultContainer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+        {/* <MainNavigation />
+          <PageLayout> */}
+          <Router>
+              <Switch>
+                <Route exact path="/" component={DefaultContainer} />
+                {/* <Route exact path="/dashboard" component={Dashboard} />
+                <Route path="/profile" component={ProfileServiceContainer} /> */}
+              </Switch>
+          </Router>
+        {/* </PageLayout> */}
+    </React.Fragment>
   );
 }
 
---
+++ frontend/src/Containers/DefaultContainer.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+// import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import { Carousel } from 'react-responsive-carousel';
+
+
+
+class DefaultContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h3><font color="red">Welcome to My Profile</font></h3>
+                {/* <Carousel autoPlay>
+                    <div>
+                        <img src="/img/1.png" alt="site" />
+                    </div>
+                    <div>
+                        <img src="/img/2.png" alt="site" />
+                    </div>
+                    <div>
+                        <img src="/img/3.png" alt="site" />
+                    </div>
+                </Carousel> */}
+            </div>
+        )
+    }
+}
+
+export default DefaultContainer
