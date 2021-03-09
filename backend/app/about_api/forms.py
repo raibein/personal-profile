0f2f35888ff39069a531e6bf30eb5552fbac3f71
raben shrestha <raben@raben.ntbtoshiba.wifi.nat>
2021-03-09 13:39:14 +0100

    CRUD for about page

--- backend/app/about_api/forms.py
+++ backend/app/about_api/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 
 class AboutForm(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
-    description = StringField('description', validators=[DataRequired()])
+    description = TextAreaField('description')
 
 class DeleteForm(FlaskForm):
     pass
--- backend/app/about_api/routes.py
+++ backend/app/about_api/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, json, jsonify
 
 from app.about_api.models import About
-from app.about_api.forms import AboutForm
+from app.about_api.forms import AboutForm, DeleteForm
 
 from app import db
 
@@ -13,7 +13,7 @@ about = Blueprint(
 
 
 
-@about.route("/", methods=['GET', 'POST'])
+@about.route("/", methods=['GET', 'POST', 'PATCH'])
 def index():
     
     if request.method == "POST":
@@ -31,16 +31,16 @@ def index():
         })
         # return render_template('about-new.html', form=form)
 
-    return jsonify([
-        {
-            'id': about_object.id, 
-            'title': about_object.title, 
-            'description': about_object.description
-        }
-        for about_object in About.query.all()
-    ])
+    # return jsonify([
+    #     {
+    #         'id': about_object.id, 
+    #         'title': about_object.title, 
+    #         'description': about_object.description
+    #     }
+    #     for about_object in About.query.all()
+    # ])
 
-    # return render_template('index.html', about=About.query.all()) # fetch all data and display
+    return render_template('index.html', about=About.query.all()) # fetch all data and display
 
 
 @about.route("/about-us", methods=['GET'])
@@ -52,3 +52,30 @@ def about_page():
 def new():
     form = AboutForm()
     return render_template('about-new.html', form=form)
+
+
+@about.route('/<int:id>/edit', methods=["GET", "POST"])
+def edit(id):
+    about=About.query.get(id)
+    form = AboutForm(obj=about)
+
+    if request.method == "POST":
+        about.title = request.form['title']
+        about.description = request.form['description']
+        db.session.add(about)
+        db.session.commit()
+        return redirect(url_for('about.index'))
+
+    return render_template('about-edit.html', form=form, about=about)
+
+
+@about.route('/<int:id>/delete', methods =["GET", "POST"])
+def delete(id):
+    about=About.query.get(id)
+    if request.method == "POST":
+        # form = DeleteForm(request.form)
+        # if form.validate():
+        db.session.delete(about)
+        db.session.commit()
+        return redirect(url_for('about.index'))
+    return render_template('index.html', about=About.query.all())
